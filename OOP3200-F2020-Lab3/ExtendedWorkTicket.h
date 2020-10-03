d9eb06b5098789b027a940572514f528ba177b75
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-10-03 17:35:43 -0400

    Added commenting, looks pretty much done

--- OOP3200-F2020-Lab3/ExtendedWorkTicket.h
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.h
@@ -1,7 +1,7 @@
 /* Program Name: OOP 3200 - Lab 3 - Inheritance
  * Authors: Ryan Clayson and Daniel Hinbest
  * Date: October 4, 2020
- * Description:
+ * Description: A child class of the WorkTicket parent class that allows the opening and closing of tickets
  */
 #pragma once
 
@@ -14,19 +14,25 @@
 class ExtendedWorkTicket : public WorkTicket
 {
 private:
+	// Attribute of the ExtendedWorkTicket to set its open status
 	bool isOpen = true;
 
 public:
+	//Default constructor - Calls the WorkTicket and sets the isOpen value
 	ExtendedWorkTicket()
 	{
 		WorkTicket();
 		isOpen = true;
 	}
 
+	//Parameterized constructor - sets the parameters from the parent class and the isOpen parameter
+	//Initializes all the inherited attributes
 	ExtendedWorkTicket(const int ticket_number, const string& client_id, const int month, const int day, const int year, const string& description, const bool my_isOpen);
 
 	//Accessor for isOpen
 	bool IsOpen() const;
+
+	//Mutator to close the ticket
 	void CloseTicket();
 
 	//Mutator to set the work ticket
@@ -40,15 +46,8 @@ public:
 
 
 };
-/* Program Name: OOP 3200 - Lab 3 - Inheritance
- * Authors: Ryan Clayson and Daniel Hinbest
- * Date: October 4, 2020
- * Description:
- */
 
-#include "ExtendedWorkTicket.h"
-
- //Parameterized constructor of ExtendedWorkTicket
+//Parameterized constructor of ExtendedWorkTicket
 ExtendedWorkTicket::ExtendedWorkTicket(const int ticket_number, const string& client_id, const int month, const int day,
 	const int year, const string& description, const bool my_isOpen)
 {
@@ -59,28 +58,32 @@ ExtendedWorkTicket::ExtendedWorkTicket(const int ticket_number, const string& cl
 	isOpen = my_isOpen;
 }
 
+//Accessor to return the isOpen attribute
 bool ExtendedWorkTicket::IsOpen() const
 {
 	return isOpen;
 }
 
+//Function override that calls the parent function and outputs whether it's open or closed
 void ExtendedWorkTicket::ShowWorkTicket() const
 {
 	WorkTicket::ShowWorkTicket();
 
 	if (isOpen == true)
 
-		cout << "\nTicket: OPEN" << endl;
+		cout << "Ticket:        OPEN" << endl;
 	else
 
-		cout << "\nTicket: CLOSED" << endl;
+		cout << "Ticket:        CLOSED" << endl;
 }
 
+//Mutator that closes the work ticket
 void ExtendedWorkTicket::CloseTicket()
 {
 	isOpen = false;
 }
 
+//Function overload that checks if the input is valid and will set the work ticket if it is valid
 bool ExtendedWorkTicket::SetWorkTicket(int ticket_number, const string& client_id, int day, int month, int year,
 	const string& description, const bool my_isOpen)
 {
@@ -96,25 +99,25 @@ bool ExtendedWorkTicket::SetWorkTicket(int ticket_number, const string& client_i
 	}
 	return valid;
 }
-
+// Overloaded input operator for ExtendedWorkTicket
 ostream& operator<<(ostream& out, const ExtendedWorkTicket& ticket)
 {
 	string valid;
 
 	if (ticket.isOpen == true)
 	{
-		valid = "Open";
+		valid = "OPEN";
 	}
 	else
 	{
-		valid = "Closed";
+		valid = "CLOSED";
 	}
 
 	out << (WorkTicket)ticket
-		<< "Open:" << valid << std::endl;
+		<< "Ticket:        " << valid << std::endl;
 
 	return out;
 }
-
+//end of overload
 #endif
 
--- OOP3200-F2020-Lab3/Main.cpp
+++ OOP3200-F2020-Lab3/Main.cpp
@@ -1,7 +1,7 @@
 /* Program Name: OOP 3200 - Lab 3 - Inheritance
  * Authors: Ryan Clayson and Daniel Hinbest
  * Date: October 4, 2020
- * Description: 
+ * Description: Another modification of WorkTicket with a child class of the WorkTicket parent class that allows the opening and closing of tickets
  */
 
 #include <iostream>
@@ -11,10 +11,12 @@
 
 int main()
 {
-	/* FIX THIS BY IMPLEMENTING A WORKING ExtendedWOrkTicket Class*/
+	//Default ticket1
 	ExtendedWorkTicket testTicket1;
+	//Parameters for testTicket2
 	ExtendedWorkTicket testTicket2(1, "AMCE_123", 1, 7, 2014, "Password Reset", true);
 
+	//Outputs testTicket1 & testTicket2
 	std::cout << std::endl << "As Initialized: " << std::endl;
 	testTicket1.ShowWorkTicket();
 	testTicket2.ShowWorkTicket();
@@ -30,6 +32,7 @@ int main()
 
 	std::cout << std::endl << "Ticket 2: " << testTicket2 << std::endl;
 
+	//Closes ticket2
 	testTicket2.CloseTicket();
 
 	std::cout << std::endl << "After Ticket 2 closed: " << testTicket2 << std::endl;
