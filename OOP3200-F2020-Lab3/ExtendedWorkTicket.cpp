c8ac03505e9699e63e7528459df9e45ac44323e7
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-10-03 15:24:28 -0400

    Created IsOpen accessor, both default and parameterized constructors

--- OOP3200-F2020-Lab3/ExtendedWorkTicket.cpp
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.cpp
@@ -1,1 +1,18 @@
 #include "ExtendedWorkTicket.h"
+
+ExtendedWorkTicket::ExtendedWorkTicket()
+{
+	
+}
+
+//Parameterized constructor of ExtendedWorkTicket
+ExtendedWorkTicket::ExtendedWorkTicket(const int ticket_number, const string& client_id, const int month, const int day,
+	const int year, const string& description, const bool isOpen)
+{
+	
+}
+
+bool ExtendedWorkTicket::IsOpen() const
+{
+	return isOpen;
+}
--- OOP3200-F2020-Lab3/ExtendedWorkTicket.h
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.h
@@ -9,9 +9,15 @@
 class ExtendedWorkTicket: public WorkTicket
 {
 private:
+	bool isOpen = true;
 
 public:
-	ExtendedWorkTicket()
+	ExtendedWorkTicket();
+	
+	ExtendedWorkTicket(const int ticket_number, const string& client_id, const int month, const int day, const int year, const string& description, const bool isOpen);
+
+	//Accessor for isOpen
+	bool IsOpen() const;
 };
 
 #endif
