e4a4c0e797af526383bf50c2d322451c6f7884d1
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-10-02 23:44:38 -0400

    It's a start

---
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.cpp
@@ -0,0 +1,1 @@
+#include "ExtendedWorkTicket.h"
---
+++ OOP3200-F2020-Lab3/ExtendedWorkTicket.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _EXTENDED_WORK_TICKET_H
+#define _EXTENDED_WORK_TICKET_H
+
+#include "WorkTicket.h"
+#include <string>
+
+class ExtendedWorkTicket: public WorkTicket
+{
+private:
+
+public:
+	ExtendedWorkTicket()
+};
+
+#endif
